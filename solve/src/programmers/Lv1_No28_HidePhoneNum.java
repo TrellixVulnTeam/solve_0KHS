570f527d23d1a03a6277d3fff4a633cff9c05f85
tech7billie <tech.billie@gmail.com>
2020-08-20 13:23:31 +0900

    Lv1_No.28 핸드폰 번호 가리기

--- solve/src/programmers/Lv1_No28_HidePhoneNum.java
+++ solve/src/programmers/Lv1_No28_HidePhoneNum.java
@@ -1,6 +1,8 @@
 package programmers;
 /* 
-■■■ 핸드폰 번호 가리기 ■■■
+■■■ NO.28 핸드폰 번호 가리기 ■■■
+[풀이 일자] 2020.04.22
+
 [문제 설명]
 프로그래머스 모바일은 개인정보 보호를 위해 고지서를 보낼 때 고객들의 전화번호의 일부를 가립니다.
 전화번호가 문자열 phone_number로 주어졌을 때, 전화번호의 뒷 4자리를 제외한 나머지 숫자를 전부 *으로 가린 문자열을 리턴하는 함수, solution을 완성해주세요.
@@ -14,7 +16,7 @@ phone_number	return
 027778888	
 */
 
-public class Lv1HidePhoneNum
+public class Lv1_No28_HidePhoneNum
 {
 	  public String solution(String phone_number) 
 	  {
